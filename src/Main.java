3689d037743367da88ada784ce851261ba174ba5
Oguzhan Cataltas <cataltasoguzhan@gmail.com>
2023-01-14 12:52:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int count;
+        for (int i = 2; i <= 100; i++) {
+            count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count <= 2) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+}
